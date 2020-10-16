cd543b904a241e81cc59a11c5e1ec27b3597490b
vlad_veko <veko.2001@gmail.com>
2020-10-16 21:35:49 +0300

    Thread pool added

---
+++ TaskQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace BooksLab
+{
+    public class TaskQueue
+    {
+        public delegate void TaskDelegate();
+
+        private Thread[] tasks;
+
+        private ConcurrentQueue<TaskDelegate> taskDelegates;
+
+        private bool isRunning;
+        public TaskQueue(int TasksCount)
+        {
+            this.tasks = new Thread[TasksCount];
+            this.taskDelegates = new ConcurrentQueue<TaskDelegate>();
+            this.isRunning = true;
+
+            for (int i = 0; i < TasksCount; i++)
+            {
+                Thread newThread = new Thread(this.AwaitLoop);
+                this.tasks[i] = newThread;
+                newThread.Start();
+            }
+
+        }
+
+        private void AwaitLoop()
+        {
+            SpinWait spinWait = new SpinWait();
+            TaskDelegate task;
+            while (this.isRunning)
+            {
+                if (taskDelegates.TryDequeue(out task))
+                    task.Invoke();
+                else
+                    spinWait.SpinOnce();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            taskDelegates.Enqueue(task);
+        }
+    }
+}
